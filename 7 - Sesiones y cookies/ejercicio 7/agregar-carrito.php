e261fdbb6de320c1b77462032b069a8f7172cffb
Tomás Malgarín <67379587+MalgaroJr@users.noreply.github.com>
2021-06-28 00:04:24 -0300

    .

---
+++ 7 - Sesiones y cookies/ejercicio 7/agregar-carrito.php
@@ -0,0 +1,15 @@
+<?php
+session_start();
+extract($_REQUEST);
+include("conexion.inc");
+if (!isset($cantidad)) {
+    $cantidad = 1;
+}
+$qry = mysqli_query($link, "select * from productos where id='" . $id . "'");
+$row = mysqli_fetch_array($qry);
+if (isset($_SESSION['carro']))
+    $carro = $_SESSION['carro'];
+$carro[md5($id)] = array('identificador' => md5($id), 'cantidad' => $cantidad, 'producto' => $row['producto'], 'precio' => $row['precio'], 'id' => $id);
+$_SESSION['carro'] = $carro;
+header("Location:catalogo.php?" . SID);
+?> 
