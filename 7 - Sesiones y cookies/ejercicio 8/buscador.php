6c9ca1efad1703478265c47ebdb699a9adff7dcb
Tomás Malgarín <67379587+MalgaroJr@users.noreply.github.com>
2021-06-28 00:20:10 -0300

    .

---
+++ 7 - Sesiones y cookies/ejercicio 8/buscador.php
@@ -0,0 +1,31 @@
+<html>
+
+<head>
+    <meta http‐equiv="Content‐Type" content="text/html; charset=utf‐8" />
+    <title>Untitled Document</title>
+</head>
+
+<body>
+    <?php
+    include("Conexion.inc");
+    $pal = $_POST['Palabra'];
+    if ($pal) {
+        $resp = mysqli_query($link,"select * from buscador where canciones LIKE '%" . $pal . "%'") or die(mysqli_error($link));
+        if (mysqli_num_rows($resp) == "0") {
+            echo "No hay resultados respecto a la palabra que busca.";
+        } else {
+            echo "<center><strong>RESULTADOS DE BUSQUEDA</strong></center><br>";
+            while ($cat = mysqli_fetch_array($resp)) { ?>
+                <td><?php echo ($cat['canciones']); ?></td>
+
+                </tr>
+                <tr>
+                    <td colspan="5">
+            <?php                 }
+        }
+    } else {
+        echo "<form name='FormBuscador' method='post' action=''><input name='Palabra' type='text' id='Palabra'><input type='submit' name='Submit' value='Buscar!'></form>";
+    } ?>
+</body>
+
+</html>
